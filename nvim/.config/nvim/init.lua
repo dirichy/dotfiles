0cf64fbb88f9813180da1dc5e619bd8ce178c3f5
dirichy <byl315121270@gmail.com>
2025-03-08 09:24:34 +0800

    add support for lilypond

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -18,3 +18,26 @@ require("lazy_nvim")
 require("keymaps")
 require("autocmds")
 --require("chinese.im")
+local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+parser_config.lilypond = {
+	install_info = {
+		url = "https://github.com/nwhetsell/tree-sitter-lilypond", -- local path or git repo
+		files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+		-- optional entries:
+		branch = "main", -- default branch in case of git repo if different from master
+		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+	},
+	-- filetype = "zu", -- if filetype does not match the parser name
+}
+parser_config.lilypond_scheme = {
+	install_info = {
+		url = "https://github.com/nwhetsell/tree-sitter-lilypond-scheme", -- local path or git repo
+		files = { "src/parser.c" }, -- note that some parsers also require src/scanner.c or src/scanner.cc
+		-- optional entries:
+		branch = "main", -- default branch in case of git repo if different from master
+		generate_requires_npm = false, -- if stand-alone parser without npm dependencies
+		requires_generate_from_grammar = false, -- if folder contains pre-generated src/parser.c
+	},
+	-- filetype = "zu", -- if filetype does not match the parser name
+}
