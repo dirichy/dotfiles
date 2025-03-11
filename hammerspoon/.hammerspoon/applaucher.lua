e31b7e36360a2a30ff58fca3386a44849bde0a96
dirichy <byl315121270@gmail.com>
2025-03-11 16:44:55 +0800

    add VLC in applaucher, add app laucher modifier two finger on touchpad

--- hammerspoon/.hammerspoon/applaucher.lua
+++ hammerspoon/.hammerspoon/applaucher.lua
@@ -11,15 +11,18 @@ local shortcuts = {
 	s = "Steam.app",
 	t = "Telegram.app",
 	w = "WeChat.app",
+	v = "VLC.app",
 	spacebar = "Alfred 5.app",
 }
 local JSON = require("JSON")
 local applaucher = {
 	title = "applaucher",
-	rules = { {
-		description = "Use thumb_on_touch+key to open app",
-		manipulators = {},
-	} },
+	rules = {
+		{
+			description = "Use thumb_on_touch+key or two finger on touch + key to open app",
+			manipulators = {},
+		},
+	},
 }
 for key, value in pairs(shortcuts) do
 	local mani = {
@@ -28,7 +31,14 @@ for key, value in pairs(shortcuts) do
 		conditions = { { type = "variable_if", name = "multitouch_extension_palm_count_total", value = 1 } },
 		type = "basic",
 	}
+	local mani2 = {
+		from = { key_code = key },
+		to = { shell_command = 'open -a "' .. value .. '"' },
+		conditions = { { type = "variable_if", name = "multitouch_extension_finger_count_total", value = 2 } },
+		type = "basic",
+	}
 	applaucher.rules[1].manipulators[#applaucher.rules[1].manipulators + 1] = mani
+	applaucher.rules[1].manipulators[#applaucher.rules[1].manipulators + 1] = mani2
 end
 local file = io.open(os.getenv("HOME") .. "/.config/karabiner/assets/complex_modifications/applaucher.json", "w")
 file:write(JSON:encode(applaucher))
