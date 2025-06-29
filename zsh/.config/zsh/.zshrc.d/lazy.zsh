615cb12f94b5f78ff639e9327c38ff000a84d77f
dirichy <byl315121270@gmail.com>
2025-06-29 08:37:06 +0800

    update

--- zsh/.config/zsh/.zshrc.d/lazy.zsh
+++ zsh/.config/zsh/.zshrc.d/lazy.zsh
@@ -1,5 +1,8 @@
 function __setup_conda(){
-  eval "$('/Users/dirichy/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
-  export PATH="/Users/dirichy/miniconda3/bin:$PATH"
+    eval "$('/Users/dirichy/miniconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+    export PATH="/Users/dirichy/miniconda3/bin:$PATH"
 }
 lazyload conda -- '__setup_conda'
+lazyload python -- '__setup_conda'
+lazyload python3 -- '__setup_conda'
+lazyload bypy -- '__setup_conda'
