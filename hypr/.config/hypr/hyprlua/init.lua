299e0410ce6c8f7bd4b3d97b20f7781027f9d131
dirichy <byl315121270@gmail.com>
2025-10-27 16:15:41 +0800

    do everything after uv.run

--- hypr/.config/hypr/hyprlua/init.lua
+++ hypr/.config/hypr/hyprlua/init.lua
@@ -1,9 +1,17 @@
-#!/usr/bin/env lua
+#!/usr/bin/luajit
 hypr = {}
-hypr.window = require("window")
-hypr.monitor = require("system.monitor")
-hypr.event = require("event")
-hypr.cmd = require("util").cmd
-hypr.dispatch = require("util").dispatch
-hypr.debounce = require("util").debounce
+local uv = require("luv")
+local check = uv.new_check()
+uv.check_start(check, function()
+	print("Executed right after uv.run starts")
+	hypr.window = require("window")
+	hypr.monitor = require("system.monitor")
+	hypr.event = require("event")
+	hypr.cmd = require("util").cmd
+	hypr.dispatch = require("util").dispatch
+	hypr.debounce = require("util").debounce
+	hypr.keybind = require("keybind")
+	pcall(require, "config")
+	uv.check_stop(check) -- 只执行一次
+end)
 require("ipcuv")
