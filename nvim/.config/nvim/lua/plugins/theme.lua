0571a335045653f099e2c55f5441053e80b62e07
dirichy <byl315121270@gmail.com>
2025-11-16 23:22:04 +0800

    add two color scheme for neovim

--- nvim/.config/nvim/lua/plugins/theme.lua
+++ nvim/.config/nvim/lua/plugins/theme.lua
@@ -1,5 +1,7 @@
 ---@alias buffer integer
 return {
+	{ "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+	{ "olimorris/onedarkpro.nvim", priority = 1000 },
 	{
 		"folke/tokyonight.nvim",
 		priority = 999,
