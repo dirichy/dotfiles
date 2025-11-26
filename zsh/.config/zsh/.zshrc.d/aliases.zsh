d448053afe0b5302d76e25424815eb11589e5829
dirichy <byl315121270@gmail.com>
2025-11-26 10:57:53 +0800

    use s as alias of ssh

--- zsh/.config/zsh/.zshrc.d/aliases.zsh
+++ zsh/.config/zsh/.zshrc.d/aliases.zsh
@@ -64,10 +64,3 @@ function cd() {
 
 alias vlc="LANG=zh_CN.UTF-8 /Applications/VLC.app/Contents/MacOS/VLC"
 alias sshfsnas="sshfs -o follow_symlinks ldirichy:/home/dirichy /Users/dirichy/ldirichy"
-sshx(){
-    autossh -M 0 \
-        -o "ServerAliveInterval=10" \
-        -o "ServerAliveCountMax=3" \
-        -t "$@" \
-        "$SHELL -lc \"tmux new -A -s ssh\" || $SHELL -l"
-}
