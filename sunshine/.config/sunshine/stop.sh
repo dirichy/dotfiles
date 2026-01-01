cc5d906a90c0df0af0766748d8bebafc004030fa
dirichy <byl315121270@gmail.com>
2026-01-02 01:23:22 +0800

    sunshine: add sunshine config

---
+++ sunshine/.config/sunshine/stop.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/zsh
+/home/dirichy/.local/bin/hyprlua 'hypr.keyword.monitor("DP-1","preferred","0x0","2.5")'
+/home/dirichy/.local/bin/hyprlua 'hypr.keyword.monitor("HDMI-A-1","disabled")'
